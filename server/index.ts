c6066cb69dce4ab54c1a5c82da1bf4cd39aae05d
Prog24 <kenta@suzuki.dev>
2022-06-18 18:35:47 +0900

    fix api

--- server/index.ts
+++ server/index.ts
@@ -1,32 +1,55 @@
 import express, { json, urlencoded, Request, Response } from 'express'
-import request from 'request'
-const jwt = require('jsonwebtoken')
+import { isAvailable, project } from 'gcp-metadata'
+import { OAuth2Client } from 'google-auth-library'
 
 const app = express()
 app.use(json())
 app.use(urlencoded({ extended: true }))
 
-const getPublicKey = (token: string) => {
-  return new Promise((resolve) => {
-    request(
-      {
-        url: 'https://www.gstatic.com/iap/verify/public_key',
-        method: 'GET',
-        json: true,
-      },
-      (_: any, res: any) => {
-        resolve(res.body[jwt.decode(token, { complete: true }).header.kid])
-      }
-    )
-  })
+const oAuth2Client = new OAuth2Client()
+
+let aud: any
+
+const audience = async () => {
+  if (!aud && (await isAvailable())) {
+    const projectNumber = await project('numeric-project-id')
+    const projectId = await project('project-id')
+    aud = '/projects/' + projectNumber + '/apps/' + projectId
+  }
+  return aud
+}
+
+const validateAssertion = async (assertion: any) => {
+  if (!assertion) {
+    return {}
+  }
+  const aud = await audience()
+
+  const response = await oAuth2Client.getIapPublicKeys()
+  const ticket = await oAuth2Client.verifySignedJwtWithCertsAsync(
+    assertion,
+    response.pubkeys,
+    aud,
+    ['https://cloud.google.com/iap']
+  )
+  const payload = ticket.getPayload()
+  return {
+    email: payload?.email,
+    sub: payload?.sub,
+  }
 }
 
 app.get('/api/ping', async (request: Request, response: Response) => {
-  const token = request.headers['x-goog-iap-jwt-assertion']
-  const publicKey = await getPublicKey(token as string)
-  jwt.verify(token, publicKey, (_: any, decoded: any) => {
-    response.json({ jwt: token, dec: decoded })
-  })
+  const assertion = request.header('X-Goo-IAP-JWT-Assertion')
+  let email: string | undefined = 'None'
+  try {
+    const info = await validateAssertion(assertion)
+    email = info.email
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error)
+  }
+  response.json({ message: email })
 })
 
 export default app
