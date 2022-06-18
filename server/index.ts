0ec70681810a604efe1f63216a6326a72480db01
Prog24 <kenta@suzuki.dev>
2022-06-18 15:34:14 +0900

    fix

--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,32 @@
 import express, { json, urlencoded, Request, Response } from 'express'
+import request from 'request'
+const jwt = require('jsonwebtoken')
 
 const app = express()
 app.use(json())
 app.use(urlencoded({ extended: true }))
 
-// const getPublicKey = (token: string) => {
-//   return new Promise(resolve => {
-//     request({
-//       url: "https://www.gstatic.com/iap/verify/public_key",
-//       method: "GET",
-//       json: true
-//     }, (_, res) => {
-//       // eslint-disable-next-line import/no-named-as-default-member
-//       resolve(res.body[jwt.decode(token, { complete: true }).header.kid])
-//     })
-//   })
-// }
+const getPublicKey = (token: string) => {
+  return new Promise((resolve) => {
+    request(
+      {
+        url: 'https://www.gstatic.com/iap/verify/public_key',
+        method: 'GET',
+        json: true,
+      },
+      (_: any, res: any) => {
+        resolve(res.body[jwt.decode(token, { complete: true }).header.kid])
+      }
+    )
+  })
+}
 
-app.get('/api/ping', (request: Request, response: Response) => {
+app.get('/api/ping', async (request: Request, response: Response) => {
   const token = request.headers['x-goog-iap-jwt-assertion']
-  response.json({ jwt: token })
+  const publicKey = await getPublicKey(token as string)
+  jwt.verify(token, publicKey, (_: any, decoded: any) => {
+    response.json({ jwt: token, dec: decoded })
+  })
 })
 
 export default app
