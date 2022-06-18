a37bdea45d8b0b494b5a694c5c213d4efd3d08c4
Prog24 <kenta@suzuki.dev>
2022-06-18 13:51:19 +0900

    add server

---
+++ server/index.ts
@@ -0,0 +1,23 @@
+import express, { json, urlencoded, Request, Response } from 'express'
+
+const app = express()
+app.use(json())
+app.use(urlencoded({ extended: true }))
+
+// const getPublicKey = (token: string) => {
+//   return new Promise(resolve => {
+//     request({
+//       url: "https://www.gstatic.com/iap/verify/public_key",
+//       method: "GET",
+//       json: true
+//     }, (_, res) => {
+//       // eslint-disable-next-line import/no-named-as-default-member
+//       resolve(res.body[jwt.decode(token, { complete: true }).header.kid])
+//     })
+//   })
+// }
+
+app.get('/api/ping', (request: Request, response: Response) => {
+  const token = request.headers['x-goog-iap-jwt-assertion']
+  response.json({ jwt: token })
+})
